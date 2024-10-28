a95c239232354521d2e5a3bb69cef2d76605beb5
Shivam Saxena <shivam2003official@gmail.com>
2024-10-28 10:34:05 +0530

    Home Page Done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,12 @@ export default function page() {
 			<div className="w-full lg:w-1/2 p-10 flex items-center justify-center">
 				<Image src={Logo} height={175} width={175} className="hover:scale-125 h-fit w-fit duration-300" alt="Tech Revolution Conference" />
 			</div>
-			<div className="flex p-5 w-full lg:w-1/2 flex-col gap-4 font-semibold">
+			<div className="flex p-5 w-full lg:w-1/2 lg:text-left text-center flex-col gap-4 font-semibold">
 				<h6 className="font-semibold text-xl">#CYBERCOM24 (IEEE Record. 63683)</h6>
 				<h1 className="text-xl font-semibold"><span className="font-semibold text-6xl uppercase">International Conference</span><br /> on Tech Revolution</h1>
 				<h2>15-16, November, 2024</h2>
 				<h3>HYBRID MODE</h3>
-				<div className="lg:w-1/3 w-full flex-wrap pr-5 flex text-white justify-between items-center">
+				<div className="w-fit flex-nowrap gap-10 pr-5 flex text-white justify-between items-center">
 					<Link href="/about" className="px-5 hover:text-black hover:bg-white duration-300 text-semibold py-3 rounded-[25px] bg-sky-700">About</Link>
 					<Link href="/tracks"  className="px-5 w-fit py-3 hover:translate-x-5 hover:text-black duration-300">Tracks →</Link>
 				</div>
