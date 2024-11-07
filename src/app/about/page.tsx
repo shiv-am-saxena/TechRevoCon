3da8e764fd4d9a53863573b92ab5e8c0045a3e33
Shivam Saxena <shivam2003official@gmail.com>
2024-11-07 19:44:41 +0530

    Site Done

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -27,7 +27,7 @@ export default function page() {
 					</div>
 					<Image src={srmu} height={350} width={600} className="order-1 lg:order-2 rounded-xl" alt="Shri Ramswaroop Memorial University" />
 				</div>
-				<div className="w-full flex flex-col lg:flex-row p-10 gap-7 items-center justify-around rounded-3xl">
+				<div className="w-full flex flex-col lg:flex-row p-5 gap-7 items-center justify-around rounded-3xl">
 					<div className="order-2 w-full lg:w-1/2">
 						<h3 className="text-2xl font-bold uppercase leading-10 tracking-widest">
 							Computer Science and Engineering
@@ -44,7 +44,7 @@ export default function page() {
 					<Image src={srmu} height={350} width={600} className="order-1 rounded-xl" alt="Shri Ramswaroop Memorial University" />
 				</div>
 				<hr />
-				<div className="w-full flex flex-col px-10 pt-5 gap-7 items-center justify-around">
+				<div className="w-full flex flex-col px-5 pt-5 gap-7 items-center justify-around">
 					<iframe
 						src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3556.463069273348!2d81.09534802544081!3d26.952231326623643!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39995ee81add328f%3A0xbe8acc99218572c9!2sShri%20Ramswaroop%20Memorial%20University%20(SRMU)!5e0!3m2!1sen!2sin!4v1730651130528!5m2!1sen!2sin"
 						width={1200}
